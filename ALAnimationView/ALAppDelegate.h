13954bafe56225af7cd9e11f7cdf35a9f035cb46
Alex Leite <admin@al7dev.com>
2014-03-20 14:53:30 -0500

    Initial Commit

---
+++ ALAnimationView/ALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALAppDelegate.h
+//  ALAnimationView
+//
+//  Created by Alexandre Leite on 3/20/14.
+//  Copyright (c) 2014 al7dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ALAnimationView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ALAnimationView
+//
+//  Created by Alexandre Leite on 3/20/14.
+//  Copyright (c) 2014 al7dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ALAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ALAppDelegate class]));
+    }
+}
